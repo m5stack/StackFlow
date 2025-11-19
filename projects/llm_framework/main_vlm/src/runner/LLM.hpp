164479ae6756436a18df9b454a00035b7ae008dc
LittleMouse <niuchenyang@m5stack.com>
2025-11-19 09:54:07 +0800

    [update] update cosy_voice,  llm, vlm version

--- projects/llm_framework/main_vlm/src/runner/LLM.hpp
+++ projects/llm_framework/main_vlm/src/runner/LLM.hpp
@@ -1624,10 +1624,28 @@ public:
         }
         printf("¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯\n" MACRO_END);
 
+        nlohmann::json dynamic_config;
+
+        dynamic_config["enable_temperature"] = _attr.enable_temperature;
+        dynamic_config["temperature"]        = _attr.temperature;
+
+        dynamic_config["enable_repetition_penalty"] = _attr.enable_repetition_penalty;
+        dynamic_config["repetition_penalty"]        = _attr.repetition_penalty;
+        dynamic_config["penalty_window"]            = _attr.penalty_window;
+
+        dynamic_config["enable_top_p_sampling"] = _attr.enable_top_p_sampling;
+        dynamic_config["top_p"]                 = _attr.top_p;
+
+        dynamic_config["enable_top_k_sampling"] = _attr.enable_top_k_sampling;
+        dynamic_config["top_k"]                 = _attr.top_k;
+
         if (!postprocess.load_config(attr.post_config_path)) {
             ALOGW("load postprocess config(%s) failed", attr.post_config_path.c_str());
         }
 
+        if (!postprocess.load_config(dynamic_config)) {
+            ALOGW("load postprocess config(%s) failed", dynamic_config.dump(4).c_str());
+        }
         // Reset();
         ALOGI("LLM init ok");
         return true;
