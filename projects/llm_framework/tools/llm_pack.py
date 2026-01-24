854a31f2b3da6355560917dcf06e2c44f40cda8c
LittleMouse <niuchenyang@m5stack.com>
2026-01-24 10:53:00 +0800

    [update] update llm-asr

--- projects/llm_framework/tools/llm_pack.py
+++ projects/llm_framework/tools/llm_pack.py
@@ -376,7 +376,7 @@ if __name__ == "__main__":
         'llm-sys':[create_bin_deb,'llm-sys', '1.7', src_folder, revision],
         'llm-audio':[create_bin_deb,'llm-audio', '1.8', src_folder, revision],
         'llm-kws':[create_bin_deb,'llm-kws', '1.9', src_folder, revision],
-        'llm-asr':[create_bin_deb,'llm-asr', '1.8', src_folder, revision],
+        'llm-asr':[create_bin_deb,'llm-asr', '1.9', src_folder, revision],
         'llm-llm':[create_bin_deb,'llm-llm', '1.12', src_folder, revision],
         'llm-tts':[create_bin_deb,'llm-tts', '1.6', src_folder, revision],
         'llm-melotts':[create_bin_deb,'llm-melotts', '1.9', src_folder, revision],
